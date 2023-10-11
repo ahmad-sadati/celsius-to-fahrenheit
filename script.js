b298d56f02e3eb5168eeddd06b0827391e18f3d7
Ahmad Sadati <a.sadati336@gmail.com>
2023-10-11 16:50:54 +0330

    matched js with html changes and kelvin

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function temperatureConverter1(valNum) {
-    valNum = parseFloat(valNum);
-    document.getElementById("outputFahrenheit").innerHTML=(valNum*1.8)+32;
+
+function temperatureConverter(source,valNum) {
+  valNum = parseFloat(valNum);
+  var inputFahrenheit = document.getElementById("inputFahrenheit");
+  var inputCelsius = document.getElementById("inputCelsius");
+  var inputKelvin = document.getElementById("inputKelvin");
+  if (source=="inputFahrenheit") {
+    inputCelsius.value=((valNum-32)/1.8).toFixed(2);
+    inputKelvin.value=(((valNum-32)/1.8)+273.15).toFixed(2);
   }
-  function temperatureConverter2(valNum) {
-    valNum = parseFloat(valNum);
-    document.getElementById("outputCelcius").innerHTML=(valNum-32)/1.8;
+  if (source=="inputCelsius") {
+    inputFahrenheit.value=((valNum*1.8)+32).toFixed(2);
+    inputKelvin.value=((valNum)+273.15).toFixed(2);
   }
+  if (source=="inputKelvin") {
+    inputFahrenheit.value=(((valNum-273.15)*1.8)+32).toFixed(2);
+    inputCelsius.value=((valNum)-273.15).toFixed(2);
+  }
+}
